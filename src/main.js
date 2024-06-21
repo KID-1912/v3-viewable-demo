833a38297960411119786186420c37a737e35261
heyutongxue <heyutongxue@gmail.com>
2024-06-21 18:58:58 +0800

    initial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "virtual:windi.css";
+import "@/assets/styles/reset.css";
+import "@/assets/styles/global.scss";
+import SvgIcon from "@/components/SvgIcon/SvgIcon.js";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.component("SvgIcon", SvgIcon);
+app.mount("#app");
